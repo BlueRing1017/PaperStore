b05bf0fd48e2908ddbb0ba7423f83af339b9f706
BlueRing <73734435+BlueRing1017@users.noreply.github.com>
2021-06-13 22:26:23 +0900

    Initial commit

---
+++ src/main/java/template/untitle/Untitle.java
@@ -0,0 +1,17 @@
+package template.untitle;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Untitle extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
